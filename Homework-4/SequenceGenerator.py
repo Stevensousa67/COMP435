0b227f6a54d83355db80e91137c9c836ebbe8d0b
Steven Sousa <silver.steven67@gmail.com>
2023-10-31 08:31:13 -0400

    Homework 4 initiated

---
+++ Homework-4/SequenceGenerator.py
@@ -0,0 +1,10 @@
+"""
+Author: Steven Sousa
+Institution: Bridgewater State University - Analysis of Algorithms Section 02
+Instructor: Dr. Haleh Khojasteh
+Version: 1
+Release Date: October 2023
+
+Objective: This dependency will ask the user to provide a sequence of numbers that will be sorted by choosing either
+Counting Sort or Bucket Sort.
+"""
---
+++ Homework-4/main.py
@@ -0,0 +1,18 @@
+"""
+Author: Steven Sousa
+Institution: Bridgewater State University - Analysis of Algorithms Section 02
+Instructor: Dr. Haleh Khojasteh
+Version: 1
+Release Date: October 2023
+
+Objective: This project is an implementation of Radix Sort. This is accomplished by either using Counting Sort or
+Bucket Sort. It is assumed that the maximum length of the numbers to be sorted is 4 digits. If using Counting Sort,
+the digit range is between 0 and 9 included. If using Bucket Sort, there will be 10 buckets labeled 0 to 9.
+
+Observations: Make sure all the dependencies of this project are together in order for it to work. Program operates
+accomplished the desired outcome.
+
+Instructions: Run the program and follow the prompts. You will be asked to provide a sequence of numbers to be sorted
+and then select which method you would like to use (Counting Sort or Bucket Sort) and the program will apply the correct
+sorting method to your sequence, displaying the sorted sequence at the end.
+"""
